537d9582aa0a891aedef4022fd9fbb4073e37100
unknown <49073682+PiKaChu-wcg@users.noreply.github.com>
2021-03-24 21:03:12 +0800

    release 1

--- PCB.py
+++ PCB.py
@@ -3,19 +3,47 @@ import pandas as pd
 
 class PCBs:
     def __init__(self):
-        self.pcb=pd.DataFrame(columns=['id','need_time','arr_time','resp_ratio','priority','rest_time'])
-        self.now_time=0
-    def importdata(self,path):
-        self.pcb=pd.read_csv(path)
-    def exportdata(self,path):
-        self.pcb.to_csv(path,index=False)
-    def add(self,id,nt,at,rr,pr):
-        if id in self.pcb['id']:
-            return False 
-        self.pcb.loc[self.pcb.shape]=[id,nt,at,rr,pr,nt]
-        return True
-    def dele(self):
-        self.pcb=self.pcb.drop(0)
-        self.pcb.index=[i for i in range(self.pcb.shape[0])]
+        self.pcb=[pd.DataFrame(columns=['id','wait_time','rest_time','response_ratio','priority'],dtype=np.int32),
+                pd.DataFrame(columns=['id','wait_time','rest_time','response_ratio','priority'],dtype=np.int32)]
+        self.alg=1;
+    def timepass(self):
+        self.pcb[0]['wait_time']+=1
+        try:
+            self.pcb[0].loc[0,'rest_time']-=1
+            if self.pcb[0].loc[0,'rest_time']==0:
+                self.pcb[0]=self.pcb[0].drop(0)
+                self.pcb[0].index=[i for i in range(self.pcb[0].shape[0])]
+        except :
+            pass
+        self.pcb[1]['wait_time']-=1
+        self.updata()
     def updata(self):
-        self.pcb['resp_ratio']=(self.pcb['need_time']+self.now_time-self.pcb['arr_time'])/self.pcb['need_time']
+        for j in range(2):
+            self.pcb[j].index=[i for i in range(self.pcb[j].shape[0])]
+        self.push()
+        for i in range(2):
+            self.pcb[i]['response_ratio']=(self.pcb[i]['rest_time']+self.pcb[i]['wait_time'])/self.pcb[i]['wait_time']
+        self.pcb[0]=self.pcb[0].sort_values(by=self.pcb[0].columns[self.alg],ascending=True if self.alg==2 else False)
+        self.pcb[1]=self.pcb[1].sort_values(by='wait_time',ascending=True)
+        for j in range(2):
+            self.pcb[j].index=[i for i in range(self.pcb[j].shape[0])]
+
+    def push(self):
+        try:
+            while self.pcb[1].loc[0,'wait_time']==0:
+                self.pcb[0].loc[self.pcb[0].shape[0]]=self.pcb[1].loc[0]
+                self.pcb[1]=self.pcb[1].drop(0)
+                self.pcb[1].index=[i for i in range(self.pcb[1].shape[0])]
+        except :
+            pass
+        try:
+            if self.pcb[0].loc[0,'rest_time']==0:
+                self.pcb[0].drop(0)
+                self.pcb[0].index=[i for i in range(self.pcb[0].shape[0])]
+        except :
+            pass
+
+    def add(self,id,wt,rt,pr):
+        self.pcb[1].loc[self.pcb[1].shape[0]]=[id,wt,rt,0,pr]
+        self.updata()
+
--- process_scheduling.py
+++ process_scheduling.py
@@ -6,7 +6,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from qdarkstyle import load_stylesheet_pyqt5
-
+import time
 
 class QtTable(QAbstractTableModel):
     def __init__(self, data):
@@ -30,13 +30,15 @@ class mywin(QMainWindow):
     def __init__(self,shape):
         super().__init__()
         self.pcb=PCBs()
-        self.alg=1
         self.initUI(shape)
     def initUI(self,shape):
         self.setGeometry(300, 300, *shape)
-        self.view=QTableView(self)
-        self.view.setGeometry(0,0,shape[0]*0.5,shape[1]-33)
-        self.view.setModel(QtTable(self.pcb.pcb))
+        self.view1=QTableView(self)
+        self.view1.setGeometry(0,33,shape[0]*0.5,shape[1]*0.5-33)
+        self.view1.setModel(QtTable(self.pcb.pcb[0]))
+        self.view2=QTableView(self)
+        self.view2.setGeometry(0,shape[1]*0.5,shape[0]*0.5,shape[1]*0.5)
+        self.view2.setModel(QtTable(self.pcb.pcb[1]))
         #菜单项
         menubar = self.menuBar()
         dataMenu = menubar.addMenu('&Data')
@@ -49,44 +51,92 @@ class mywin(QMainWindow):
         # 下拉框,选择算法
         algselector=QComboBox(self)
         algselector.setGeometry(shape[0]*0.5+20,50,150,20)
-        algselector.addItems(['短作业有限','先入先出','高响应比','优先级有限'])
+        algselector.addItems(['先入先出','短作业有限','高响应比','优先级有限'])
         algselector.currentIndexChanged[int].connect(self.changealg)
         
+        self.qle1=QLineEdit(self)
+        self.qle1.move(shape[0]*0.5+20,120)
+        self.qle1.resize(150,30)
+        self.qle1.setPlaceholderText("id")
 
+        self.qle2=QLineEdit(self)
+        self.qle2.move(shape[0]*0.5+20,220)
+        self.qle2.resize(150,30)
+        self.qle2.setPlaceholderText("等待时间")
 
+        self.qle3=QLineEdit(self)
+        self.qle3.move(shape[0]*0.5+20,320)
+        self.qle3.resize(150,30)
+        self.qle3.setPlaceholderText("运行时间")
+
+        self.qle4=QLineEdit(self)
+        self.qle4.move(shape[0]*0.5+20,420)
+        self.qle4.resize(150,30)
+        self.qle4.setPlaceholderText("优先级")
+
+        self.btn1=QPushButton("新增",self)
+        self.btn1.move(shape[0]*0.5+20,520)
+        self.btn1.clicked.connect(self.add)
+
+        self.btn2=QPushButton("开始",self)
+        self.btn2.move(shape[0]*0.5+20,620)
+        self.btn2.clicked.connect(self.turn)
+
+        self.interval=1000
+
+        self.timer=QTimer(self)
+        self.timer.timeout.connect(self.timeout)
+
+    def turn(self):
+        s=self.sender()
+        if s.text()=='开始':
+            self.timer.start(self.interval)
+            s.setText('暂停')
+        else:
+            self.timer.stop()
+            s.setText('开始')
+
+    def refresh(self):
+        self.view1.setModel(QtTable(self.pcb.pcb[0]))
+        self.view2.setModel(QtTable(self.pcb.pcb[1]))
     def changealg(self,i):
-        self.alg=i
+        self.pcb.alg=i+1
 
 
     def add(self):
-        id=
-        nt=
-        at=
-        rr=
-        pr=
-        self.pcb.add(id,nt,at,rr,pr)
+        id=int(self.qle1.text())
+        wt=int(self.qle2.text())
+        rt=int(self.qle3.text())
+        pr=int(self.qle4.text())                                                                                                                                                                                                                                    
+        self.pcb.add(id,wt,rt,pr)
+        self.refresh()
     def timepass(self): 
-        self.pcb.time+=1
-        self.pcb.pcb.loc[0,'rest_time']-=1
-        if self.pcb.pcb.loc[0,'rest_time']==0:
-            self.pcb.pcb.dele()
-        self.pcb.updata()
+        self.pcb.timepass()
+        self.refresh()
     # 菜单动作
     def importdata(self):
         fname = QFileDialog.getOpenFileName(self, 'Open file', '/home','csv(*.csv)')
         if fname:
             self.pcb.impdata(fname[0])
+        self.refresh()
     def exportdata(self):
         fname=QFileDialog.getSaveFileName(self,'save file','/home','csv(*.csv)')
         if fname:
             self.pcb.save_status(fname[0])
 
+    def timeout(self):
+        self.timepass()
+        self.timer.start(self.interval)
+
+    
 
 def main():
     app=QApplication(sys.argv)
     app.setStyleSheet(load_stylesheet_pyqt5())
     w=mywin((1080,720))
     w.show()
+
+
     sys.exit(app.exec_())   
     
 if __name__=="__main__":
