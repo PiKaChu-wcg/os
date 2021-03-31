6dec30aebcdf501dce821a77903b8227f5d44f8f
unknown <49073682+PiKaChu-wcg@users.noreply.github.com>
2021-03-31 16:05:13 +0800

    1

--- page.py
+++ page.py
@@ -2,59 +2,66 @@ import pandas as pd
 import numpy as np
 import os
 from information import Information
+
+
 class page:
-    def __init__(self,page_num=8,page_size=8,page_num_have=1<<4):
-        self.pn=page_num
-        self.ps=page_size
-        self.ph=page_num_have
-        idx=np.arange(1<<self.pn).reshape(1,-1)
-        add=np.zeros(1<<self.pn).reshape(1,-1)
-        last_use_time=np.zeros(1<<self.pn).reshape(1,-1)
-        enter_time=np.zeros(1<<self.pn).reshape(1,-1)
-        self.list=pd.DataFrame(np.concatenate((idx,add,enter_time,last_use_time)).T,columns=['page_idx','addr_idx','enter_time','last_use_time'],dtype=np.int32)
-    def impdata(self,path):
-        self.list=pd.read_csv(path,dtype=np.int32)
-    def save_status(self,path):
-        self.list.to_csv(path,index=False)
-    def get_one_page(self,addr):
-        page_idx=addr>>self.ps
-        if page_idx<(1<<self.pn):
-            return self.list.iloc[page_idx]
+    def __init__(self, page_num=8, page_size=8, page_num_have=2):
+        self.pn = page_num
+        self.ps = page_size
+        self.ph = page_num_have
+        idx = np.arange(1 << self.pn).reshape(1, -1)
+        add = np.zeros(1 << self.pn).reshape(1, -1)
+        last_use_time = np.zeros(1 << self.pn).reshape(1, -1)
+        enter_time = np.zeros(1 << self.pn).reshape(1, -1)
+        self.list = pd.DataFrame(np.concatenate((idx, add, enter_time, last_use_time)).T,
+                                 columns=['page_idx', 'addr_idx', 'enter_time', 'last_use_time'], dtype=np.int32)
+
+    def impdata(self, path):
+        self.list = pd.read_csv(path, dtype=np.int32)
+
+    def save_status(self, path):
+        self.list.to_csv(path, index=False)
+
+    def get_one_page(self, addr):
+        page_idx = addr >> self.ps
+        if page_idx < (1 << self.pn):
+            return 1
         else:
-            return None
-    def query(self,addr,info):
-        flag=False
-        alg=info.algorithm
-        time=info.time
-        num_use=info.addr_used
-        page_idx=addr
-        if page_idx<(1<<self.pn):
-            addr_ph=0
-            if self.list.iloc[page_idx,1]:
-                self.list.iloc[page_idx,3]=time
-                flag=True
+            return 0
+
+    def query(self, addr, info):
+        flag = False
+        alg = info.algorithm
+        time = info.time
+        num_use = info.addr_used
+        page_idx = addr
+        if page_idx < (1 << self.pn):
+            addr_ph = 0
+            if self.list.iloc[page_idx, 1]:
+                self.list.iloc[page_idx, 3] = time
+                flag = True
             else:
-                flag=False
-                if num_use==self.ph:
-                    list=self.list[self.list['addr_idx']]
-                    idx=self.swap(list,alg)
-                    idx_min=list.idxmin()
-                    addr_ph=list.iloc[idx,'addr_idx']
-                    list.iloc[idx,'addr_idx']=0
-                else :
-                    info.addr_used+=1
-                    addr_ph=info.addr_used
-                self.list.iloc[page_idx,3]=self.list.iloc[page_idx,2]=time
-                self.list.iloc[page_idx,1]=addr_ph
-        info.time+=1
+                flag = False
+                if num_use == self.ph:
+                    print(1)
+                    list = self.list.loc[self.list['addr_idx'] != 0]
+                    idx = self.swap(list, alg)
+                    print(list)
+                    addr_ph += list.loc[list.loc['addr_idx'] == idx]['addr_idx']
+                    print('addr{}'.format(addr_ph))
+                    self.list.loc[self.list['addr_idx'] == addr_ph, 'addr_idx'] = 0
+                    print(1)
+                else:
+                    info.addr_used += 1
+                    addr_ph += info.addr_used
+                self.list.iloc[page_idx, 3] = self.list.iloc[page_idx, 2] = time
+                self.list.iloc[page_idx, 1] = addr_ph
+        info.time += 1
         info.query(flag)
-        return flag
 
-        def swap(self,list,alg):
-            idx=0
-            if alg<=3:
-                idx=self.list.astype(float).idxmax()[alg]
-            return idx
-
-
-
+    def swap(self, list, alg):
+        idx = 0
+        if alg <= 3:
+            idx = list.astype(float).idxmin()[alg]
+            print(list.astype(float).idxmin(), list.iloc[idx, 1])
+        return list.iloc[idx, 1]
--- page_replacement.py
+++ page_replacement.py
@@ -13,93 +13,114 @@ class QtTable(QAbstractTableModel):
     def __init__(self, data):
         QAbstractTableModel.__init__(self)
         self._data = data
+
     def rowCount(self, parent=None):
         return self._data.shape[0]
+
     def columnCount(self, parent=None):
         return self._data.shape[1]
+
     def data(self, index, role=Qt.DisplayRole):
         if index.isValid():
             if role == Qt.DisplayRole:
                 return str(self._data.iloc[index.row(), index.column()])
         return None
+
     def headerData(self, col, orientation, role):
         if orientation == Qt.Horizontal and role == Qt.DisplayRole:
             return self._data.columns[col]
 
+
 class mywin(QMainWindow):
-    def __init__(self,shape):
+    def __init__(self, shape):
         super().__init__()
-        self.info=info()
+        self.shape = shape
+        self.info = info()
         self.initUI(shape)
-        
-    def initUI(self,shape):
+
+    def initUI(self, shape):
         self.setGeometry(300, 300, *shape)
-        #数据显示的组件
-        self.pagelist=page()
-        self.view=QTableView(self)
-        self.view.setGeometry(0,33,shape[0]*0.5,shape[1]-33)
+        # 数据显示的组件
+        self.pagelist = page()
+        self.view = QTableView(self)
+        self.view.setGeometry(0, 33, shape[0] * 0.5, shape[1] - 33)
         self.view.setModel(QtTable(self.pagelist.list))
-        #菜单项
+        # 菜单项
         menubar = self.menuBar()
         dataMenu = menubar.addMenu('&Data')
-        impAct = QAction('Import', self)    
-        expAct = QAction('Export',self)
+        impAct = QAction('Import', self)
+        expAct = QAction('Export', self)
         impAct.triggered.connect(self.importdata)
         expAct.triggered.connect(self.exportdata)
         dataMenu.addAction(impAct)
         dataMenu.addAction(expAct)
         # 下拉框,选择算法
-        algselector=QComboBox(self)
-        algselector.setGeometry(shape[0]*0.5+20,50,150,20)
-        algselector.addItems(['先入先出','最近最久未使用','简单时钟置换'])
+        algselector = QComboBox(self)
+        algselector.setGeometry(shape[0] * 0.5 + 20, 50, 150, 20)
+        algselector.addItems(['先入先出', '最近最久未使用'])
         algselector.currentIndexChanged[int].connect(self.changealg)
-        #
-        self.btn1=QPushButton("调用内存",self)
-        self.btn1.move(shape[0]*0.5+20,220)
-        self.btn1.clicked.connect(self.query)
-
-        self.qle1=QLineEdit(self)
-        self.qle1.move(shape[0]*0.5+20,120)
-        self.qle1.resize(150,30)
+        # 填入内存地址
+        self.qle1 = QLineEdit(self)
+        self.qle1.move(shape[0] * 0.5 + 20, 140)
+        self.qle1.resize(150, 30)
         self.qle1.setPlaceholderText("内存地址")
-
-        self.lb1=QLabel(self)
+        # 按钮
+        self.btn1 = QPushButton("调用内存", self)
+        self.btn1.move(shape[0] * 0.5 + 20, 240)
+        self.btn1.clicked.connect(self.query)
+        # 缺页率显示
+        self.lb1 = QLabel(self)
         self.lb1.setText("缺页率:0/0  0%")
-        self.lb1.move(shape[0]*0.5+20,320)
-        self.lb1.resize(150,30)
+        self.lb1.move(shape[0] * 0.5 + 20, 340)
+        self.lb1.resize(150, 30)
+        # 默认页长提示
+        self.lb2 = QLabel(self)
+        self.lb2.setText("默认页长：256B")
+        self.lb2.move(shape[0] * 0.5 + 20, 440)
+        self.lb2.resize(150, 30)
+        # 越界中断提示
+        self.lb3 = QLabel(self)
+        self.lb3.setText("越界中断！请重新输入")
+        self.lb3.move(shape[0] * 0.5 + 20, 540)
+        self.lb3.resize(150, 30)
+        self.lb3.setVisible(False)
 
-
-        
-    def changealg(self,i):
-        self.info.change_alg(i+2)
+    def changealg(self, i):
+        self.info.change_alg(i + 2)
 
     # 菜单动作
     def importdata(self):
-        fname = QFileDialog.getOpenFileName(self, 'Open file', '/home','csv(*.csv)')
-        if fname[0]:
+        fname = QFileDialog.getOpenFileName(self, 'Open file', '/home', 'csv(*.csv)')
+        if fname:
             self.pagelist.impdata(fname[0])
+
     def exportdata(self):
-        fname=QFileDialog.getSaveFileName(self,'save file','/home','csv(*.csv)')
-        if fname[0]:
+        fname = QFileDialog.getSaveFileName(self, 'save file', '/home', 'csv(*.csv)')
+        if fname:
             self.pagelist.save_status(fname[0])
+
     def query(self):
-        addr=int(self.qle1.text())
-        page=addr>>self.pagelist.ps
-        self.pagelist.query(page,self.info)
-        self.lb1.setText("缺页率{}/{}   {}".format(self.info.num_lack,self.info.num_query,round(self.info.num_lack/self.info.num_query*100,1)))
-        self.view.setModel(QtTable(self.pagelist.list))
-        self.view.selectRow(page)
-
-
-
+        shape = self.shape
+        addr = int(self.qle1.text())
+        if not self.pagelist.get_one_page(addr):
+            self.lb3.setVisible(True)
+        else:
+            self.lb3.setVisible(False)
+            page = addr >> self.pagelist.ps
+            self.pagelist.query(page, self.info)
+            self.lb1.setText("缺页率{}/{}   {}".format(self.info.num_lack, self.info.num_query,
+                                                    round(self.info.num_lack / self.info.num_query * 100, 1)))
+            self.view.setModel(QtTable(self.pagelist.list))
+            self.view.selectRow(page)
 
 
 def main():
-    app=QApplication(sys.argv)
+    app = QApplication(sys.argv)
     app.setStyleSheet(load_stylesheet_pyqt5())
-    w=mywin((1080,720))
+    w = mywin((1080, 720))
     w.show()
-    sys.exit(app.exec_())   
-    
-if __name__=="__main__":
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
     main()
